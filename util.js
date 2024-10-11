4bc7476a2d3037945f2a2e2f4db3aa1c394dbdf2
chenbo860826 <bo.chen@moblab.com>
2024-10-11 22:18:03 +0800

    [Improve] support new model

--- util.js
+++ util.js
@@ -23,8 +23,10 @@ let chatMeta = {
             "options": [
                 { "key": "gpt-4o-mini-2024-07-18" },
                 { "key": "gpt-4o-2024-05-13" },
+                { "key": "gpt-4o-2024-08-06" },
                 { "key": "gpt-4" },
                 { "key": "gpt-4-0613" },
+                { "key": "gpt-4-turbo" },
                 { "key": "gpt-4-1106-preview" },
                 { "key": "gpt-4-turbo-2024-04-09" },
                 { "key": "gpt-3.5-turbo" },
